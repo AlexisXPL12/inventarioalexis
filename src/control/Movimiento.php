647cb3a65c81ced8e9bcb9b2ee23ab766589af14
AlexisXPL12 <valdiviatruciosalexisgabriel@gmail.com>
2025-06-17 16:39:16 -0500

    update movimiento
    crear fucion para imprimir

--- src/control/Movimiento.php
+++ src/control/Movimiento.php
@@ -208,7 +208,20 @@ if ($tipo == "datos_registro") {
     echo json_encode($arr_Respuesta);
 }
 if ($tipo == "buscar_movimiento_id") {
+    $arr_Respuesta = array('status' => false, 'msg' => 'Error_Sesion');
     if ($objSesion->verificar_sesion_si_activa($id_sesion, $token)) {
-
+        $id_movimiento = $_REQUEST['data'];
+        $arrMovimiento = $objMovimiento ->buscarMovimientoById($id_movimiento);
+        $arrAmbOrigen = $objAmbiente->buscarAmbienteById($arrMovimiento->id_ambiente_origen);
+        $arrAmbDestino = $objAmbiente->buscarAmbienteById($arrMovimiento->id_ambiente_destino);
+        $arrUsuario = $objUsuario->buscarUsuarioById($arrMovimiento->$id_usuario_registro);
+        $arrIntitucion = $objInstitucion->buscarInstitucionById($arrMovimiento->id_ies);
     }
+    echo json_encode($arrAmbOrigen);
+    echo json_encode('<br>');
+    echo json_encode($arrAmbDestino);
+    echo json_encode('<br>');
+    echo json_encode($arrIntitucion);
+    echo json_encode('<br>');
+    echo json_encode($arrUsuario);
 }
