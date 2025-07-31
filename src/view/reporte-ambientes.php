d20e6028113014f86b8aaabcb325f4aaef9e5b07
AlexisXPL12 <valdiviatruciosalexisgabriel@gmail.com>
2025-07-31 00:16:41 -0500

    Update reporte-ambientes.php

--- src/view/reporte-ambientes.php
+++ src/view/reporte-ambientes.php
@@ -283,7 +283,7 @@ $activeWorksheet->getStyle('A' . $filaInfo)->getFont()->setBold(true);
 
 // Configurar headers para descarga directa
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment;filename="reporte_ambientes_' . date('d-m-Y_H:i:s') . '.xlsx"');
+header('Content-Disposition: attachment;filename="reporte_ambientes_' . date('d/m/Y H:i:s') . '.xlsx"');
 header('Cache-Control: max-age=0');
 header('Expires: 0');
 header('Pragma: public');
